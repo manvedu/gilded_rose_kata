2c45504de7074df7c5e3cef0d433f61350beb430
manvedu_agnes <mavelandiad@gmail.com>
2017-03-16 12:26:30 -0500

    WIP

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,58 +1,62 @@
 def update_quality(items)
-  def verify_sellin(param)
-    sell_in ={
-      "< 0" => lambda {puts "hola marce linda "},
-      "< 6"=> lambda {},
-      "< 11"=> lambda {}
-    }
-    sell_in[param].call
-  end
-
-  def verify_name(param, item)
-    names = {
-      "Sulfuras, Hand of Ragnaros"=> lambda {},
-      "Aged Brie"=> lambda { item.sell_in -= 1 },
-      "Backstage passes to a TAFKAL80ETC concert"=> lambda {item.sell_in -= 1 },
-      "NORMAL ITEM"=> lambda { item.sell_in -= 1}
-    }
-    names[param].call
-  end
-
-  def verify_quality(param)
-    quality = {
-      " > 0" => lambda {},
-      " < 50" => lambda {}
-    }
-    quality[param].call
-  end
-
   items.each do |item|
-    if item.quality > 0 and item.name != 'Sulfuras, Hand of Ragnaros' and item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      item.quality -= 1
-    end
-    if item.quality < 50 and !(item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert')
-      item.quality += 1
-      if item.sell_in < 11 and item.quality < 50 and item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        item.quality += 1
-      end
-      if item.sell_in < 6 and item.quality < 50 and item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        item.quality += 1
-      end
-    end
-
-    verify_name(item.name, item)
-
-    if item.quality > 0 and item.name != 'Sulfuras, Hand of Ragnaros' and item.name != 'Backstage passes to a TAFKAL80ETC concert' and item.name != "Aged Brie" and item.sell_in < 0
-      item.quality -= 1
-    end
-    if item.name == 'Backstage passes to a TAFKAL80ETC concert' and item.name != "Aged Brie" and item.sell_in < 0
-      item.quality = 0
-    end
-    if item.quality < 50 and item.name == "Aged Brie" and item.sell_in < 0
-      item.quality += 1
-    end
+    verify_item(item)
   end
 end
+def verify_sellin(param)
+  sell_in ={
+    "< 0" => lambda {puts "hola marce linda "},
+    "< 6"=> lambda {},
+    "< 11"=> lambda {}
+  }
+  sell_in[param].call
+end
+
+def verify_name(param, item)
+  names = {
+    "Sulfuras, Hand of Ragnaros"=> lambda {},
+    "Aged Brie"=> lambda { item.sell_in -= 1 },
+    "Backstage passes to a TAFKAL80ETC concert"=> lambda {item.sell_in -= 1 },
+    "NORMAL ITEM"=> lambda { item.sell_in -= 1}
+  }
+  names[param].call
+end
+
+def verify_quality(param)
+  quality = {
+    " > 0" => lambda {},
+    " < 50" => lambda {}
+  }
+  quality[param].call
+end
+def verify_item(item)
+  if item.quality < 50 and !(item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert')
+    item.quality += 1
+    if item.sell_in < 11 and item.quality < 50 and item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      item.quality += 1
+    end
+    if item.sell_in < 6 and item.quality < 50 and item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      item.quality += 1
+    end
+  end
+  verify_name(item.name, item)
+  if item.quality > 0 
+    if item.name == "NORMAL ITEM"
+      item.quality -= 1
+    end
+    if item.name != 'Sulfuras, Hand of Ragnaros' and item.name != 'Backstage passes to a TAFKAL80ETC concert' and item.name != "Aged Brie" and item.sell_in < 0
+      item.quality -= 1
+    end
+  end
+
+  if item.name == 'Backstage passes to a TAFKAL80ETC concert' and item.name != "Aged Brie" and item.sell_in < 0
+    item.quality = 0
+  end
+  if item.quality < 50 and item.name == "Aged Brie" and item.sell_in < 0
+    item.quality += 1
+  end
+end
+
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
