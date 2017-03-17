8ae20be28c6457e3ca18463b098233006056e86a
manvedu <mavelandiad@gmail.com>
2017-03-17 16:13:59 -0500

    WIP

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -2,17 +2,8 @@ def update_quality(items)
   items.each {|item| verify_item(item)}
 end
 
-def verify_sellin(param)
-  sell_in ={
-    "< 0" => lambda {puts "hola marce linda "},
-    "< 6"=> lambda {},
-    "< 11"=> lambda {}
-  }
-  sell_in[param].call
-end
-
-def increase_quality(item)
-  item.quality += 1
+def increase_quality(item, amount)
+  item.quality += amount
 end
 
 def decrease_quality(item, amount)
@@ -27,61 +18,51 @@ def decrease_sell_in(item)
   item.sell_in -= 1
 end
 
-def verify_quality(param)
+def verify_quality(param, item)
   quality = {
-    " > 0" => lambda {},
-    " < 50" => lambda {}
+    0 => lambda { item.quality > 0},
+    50 => lambda { item.quality < 50 }
   }
   quality[param].call
 end
 
 ESPECIAL_NAMES =[ "Sulfuras, Hand of Ragnaros", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert"]
 
+def verify_sell_in(param, item)
+  names = {
+    "Sulfuras, Hand of Ragnaros"=> lambda {},
+    "Aged Brie"=> lambda { },
+    "Backstage passes to a TAFKAL80ETC concert"=> lambda {decrease_sell_in(item) }
+  }
+  names[param] || lambda { decrease_sell_in(item)}
+end
+
 def verify_name(param, item)
   names = {
     "Sulfuras, Hand of Ragnaros"=> lambda {},
-    "Aged Brie"=> lambda { item.sell_in -= 1 },
+    "Aged Brie"=> lambda { decrease_sell_in(item)},
     "Backstage passes to a TAFKAL80ETC concert"=> lambda {decrease_sell_in(item) }
   }
-  names[param] || lambda { item.sell_in -= 1}
-end
-
-def ranger(number1,number2)
-  number1 > number2
+  names[param] || lambda { decrease_sell_in(item)}
 end
 
 def comparer_than_(number1,number2)
   number1 > number2
 end
 
-
-def not_special_name?(name)
-  !ESPECIAL_NAMES.include?(name)
-end
-
 def verify_item(item)
-  if item.quality < 50 and ESPECIAL_NAMES.last(2).include?(item.name)
-    increase_quality(item)
-    if item.sell_in < 11 and item.name == 'Backstage passes to a TAFKAL80ETC concert'
-      increase_quality(item)
-    end
-    if item.sell_in < 6 and item.name == 'Backstage passes to a TAFKAL80ETC concert'
-      increase_quality(item)
-    end
-  end
+  verify_quality(50, item) && ESPECIAL_NAMES.last(2).include?(item.name) && increase_quality(item, 1)
+  verify_quality(50, item) && ESPECIAL_NAMES.last(2).include?(item.name) && item.sell_in < 11 && ESPECIAL_NAMES.last.include?(item.name) && increase_quality(item, 1)
+  verify_quality(50, item) && ESPECIAL_NAMES.last(2).include?(item.name) && item.sell_in < 6 && ESPECIAL_NAMES.last.include?(item.name)  && increase_quality(item, 1)
 
   verify_name(item.name, item).call
 
-  decrease_quality(item, 1) if item.quality > 0 and not_special_name?(item.name) and item.sell_in > 0
-  decrease_quality(item, 2) if item.quality > 0 and not_special_name?(item.name) and item.sell_in < 0
+  verify_quality(0, item) && item.quality > 0 && !ESPECIAL_NAMES.include?(item.name) && item.sell_in > 0 && decrease_quality(item, 1)
+  verify_quality(0, item) && !ESPECIAL_NAMES.include?(item.name) && item.sell_in < 0 && decrease_quality(item, 2) 
 
-  if item.name == 'Backstage passes to a TAFKAL80ETC concert' and item.name != "Aged Brie" and item.sell_in < 0
-    set_quality_zero(item)
-  end
+  ESPECIAL_NAMES.last.include?(item.name) && item.name != "Aged Brie" && item.sell_in < 0 && set_quality_zero(item)
 
-  if item.quality < 50 and item.name == "Aged Brie" and item.sell_in < 0
-    increase_quality(item)
-  end
+  verify_quality(50, item) && item.name == "Aged Brie" && item.sell_in < 0 && increase_quality(item, 1)
 end
 
 
